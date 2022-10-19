044225c63d7ab980c933f46e3c1c2453b27d7c7b
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-19 06:22:31 +0200

    Sentiments

--- TextParser/App.swift
+++ TextParser/App.swift
@@ -6,11 +6,46 @@
 //
 
 import Foundation
+import NaturalLanguage
 
 @main
 struct App {
     static func main() {
         let text = CommandLine.arguments.dropFirst().joined(separator: " ")
         print(text)
+        
+        print()
+        
+        let sentiment = sentiment(for: text)
+        print("Sentiment analysis: \(sentiment)")
+        
+        print()
+        
+        print("Found the followun alternatives:")
+        
+        for word in text.components(separatedBy: " ") {
+            let embeddings = embeddings(for: word)
+            print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+        }
+    }
+    
+    static func sentiment(for string: String) -> Double {
+        let tagger = NLTagger(tagSchemes: [.sentimentScore])
+        tagger.string = string
+        let (sentiment, _) = tagger.tag(at: string.startIndex, unit: .paragraph, scheme: .sentimentScore)
+        return Double(sentiment?.rawValue ?? "0") ?? 0
+    }
+    
+    static func embeddings(for word: String) -> [String] {
+        var results = [String]()
+        
+        if let embedding = NLEmbedding.wordEmbedding(for: .french) {
+            let similardWords = embedding.neighbors(for: word, maximumCount: 10)
+            for word in similardWords {
+                results.append("\(word.0) has a distance of \(word.1)")
+            }
+        }
+        
+        return results
     }
 }
