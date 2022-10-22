5d438e8b25f3cd770c0c5141fdc48a1ce9fee85c
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-22 08:46:30 +0200

    Flag and options

--- TextParser/App.swift
+++ TextParser/App.swift
@@ -7,57 +7,104 @@
 
 import Foundation
 import NaturalLanguage
+import ArgumentParser
 
 @main
-struct App {
-    static func main() {
-        let text = CommandLine.arguments.dropFirst().joined(separator: " ")
-        print(text)
-        
-        let language = NLLanguageRecognizer.dominantLanguage(for: text) ?? .undetermined
-        print()
-        print("Detected language: \(language.rawValue)")
-        
-        print()
-        let sentiment = sentiment(for: text)
-        print("Sentiment analysis: \(sentiment)")
-        
-        print()
-        print("Found the following alternatives:")
-        
-        for word in text.components(separatedBy: " ") {
-            let embeddings = embeddings(for: word)
-            print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+struct App: ParsableCommand {
+    
+    static var configuration: CommandConfiguration {
+        CommandConfiguration(commandName: "analyse", abstract: "Analyse input text using a range of natural language approaches.")
+    }
+    
+    @Argument(help: "The text you want to analyse")
+    var input: [String]
+    
+    @Flag(name: .shortAndLong, help: "Show detected language.")
+    var detectLanguage = false
+
+    @Flag(name: .shortAndLong, help: "Prints how positive or negative the input is.")
+    var sentimentAnalysis = false
+
+    @Flag(name: .shortAndLong,help: "Shows the stem form of each word in the input.")
+    var lemmatize = false
+
+    @Flag(name: .shortAndLong, help: "Prints alternative words for each word in the input.")
+    var alternatives = false
+
+    @Flag(name: .shortAndLong, help: "Prints names of people, places, and organizations in the input.")
+    var names = false
+    
+    @Option(name: .shortAndLong, help: "The maximum number of alternative to suggest")
+    var maximumAlternative = 10
+     
+
+    mutating func run()  {
+        if detectLanguage == false && sentimentAnalysis == false && lemmatize == false && alternatives == false && names == false {
+            
+            // No option means all options
+            detectLanguage = true
+            sentimentAnalysis = true
+            lemmatize = true
+            alternatives = true
+            names = true
         }
         
-        print()
-        let lemma = lemmatize(string: text)
-        print("Found the following alternatives:")
-        for word in lemma {
-            let embeddings = embeddings(for: word)
-            print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+        
+        let text = input.joined(separator: " ")
+        
+        if detectLanguage {
+            let language = NLLanguageRecognizer.dominantLanguage(for: text) ?? .undetermined
+            print()
+            print("Detected language: \(language.rawValue)")
         }
         
-        print()
-        let entities = entities(for: text)
-        print ("Found the folowwing entities:")
-        for entity in entities {
-            print("\t", entity)
+        if sentimentAnalysis {
+            let sentiment = sentiment(for: text)
+            print()
+            print("Sentiment analysis: \(sentiment)")
+        }
+        
+        lazy var lemma = lemmatize(string: text)
+        
+        if alternatives {
+            print()
+            print("Found the following alternatives:")
+            
+            for word in lemma {
+                let embeddings = embeddings(for: word)
+                print("\t\(word): ", embeddings.formatted(.list(type: .and)))
+            }
+        }
+        
+        if lemmatize {
+            print()
+            print("Found the following lemma:")
+                print("\t: ", lemma.formatted(.list(type: .and)))
+            
+        }
+        
+        if names {
+            let entities = entities(for: text)
+            print()
+            print ("Found the folowwing entities:")
+            for entity in entities {
+                print("\t", entity)
+            }
         }
     }
     
-    static func sentiment(for string: String) -> Double {
+    func sentiment(for string: String) -> Double {
         let tagger = NLTagger(tagSchemes: [.sentimentScore])
         tagger.string = string
         let (sentiment, _) = tagger.tag(at: string.startIndex, unit: .paragraph, scheme: .sentimentScore)
         return Double(sentiment?.rawValue ?? "0") ?? 0
     }
     
-    static func embeddings(for word: String) -> [String] {
+    func embeddings(for word: String) -> [String] {
         var results = [String]()
         
-        if let embedding = NLEmbedding.wordEmbedding(for: .french) {
-            let similardWords = embedding.neighbors(for: word, maximumCount: 10)
+        if let embedding = NLEmbedding.wordEmbedding(for: .english) {
+            let similardWords = embedding.neighbors(for: word, maximumCount: maximumAlternative)
             for word in similardWords {
                 results.append("\(word.0) has a distance of \(word.1)")
             }
@@ -66,7 +113,7 @@ struct App {
         return results
     }
     
-    static func lemmatize(string: String) -> [String] {
+    func lemmatize(string: String) -> [String] {
         let tagger = NLTagger(tagSchemes: [.lemma])
         tagger.string = string
         var results = [String]()
@@ -88,7 +135,7 @@ struct App {
         return results
     }
     
-    static func entities(for string: String) -> [String] {
+    func entities(for string: String) -> [String] {
         let tagger = NLTagger(tagSchemes: [.nameType])
         tagger.string = string
         var results = [String]()
@@ -116,3 +163,4 @@ struct App {
         return results
     }
 }
+
